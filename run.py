aafa9996baef37d117aefcbfad634462bc6f48e8
Makoto Nakai <macofandohr1812@gmail.com>
2020-03-21 15:52:09 +0900

    removed errors

--- run.py
+++ run.py
@@ -1,21 +1,26 @@
 from flask import Flask, flash, request, redirect, url_for, render_template, send_from_directory
 from werkzeug.utils import secure_filename
-from keras.preprocessing.image import img_to_array, load_img
-from keras.models import load_model
-import tensorflow as tf
+from tensorflow.keras.preprocessing.image import img_to_array, load_img
+from tensorflow.keras.models import load_model
+
+import keras.backend.tensorflow_backend as tb
+import tensorflow.python.keras.backend as K
 import numpy as np
 import os, sys
 import cv2
 
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+tb._SYMBOLIC_SCOPE.value = True
 
 UPLOAD_FOLDER = './uploads'
 ALLOWED_EXTENSIONS = set(['png', 'jpg', 'jpeg'])
 
 HAAR_FILE = "./tools/haarcascade_frontalface_alt.xml"
 cascade = cv2.CascadeClassifier(HAAR_FILE)
+size = 50
 
 model = load_model("./tools/ladies_classification.h5", compile=False)
-graph = tf.get_default_graph()
+# graph = K.get_graph()
 
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
@@ -46,7 +51,7 @@ def predict():
             
             img = cv2.imread(filepath)
             
-            # グレースケールに変換する
+            # グレースケールに変換する?
             img_g = cv2.imread(filepath,0)
             
             # カスケード型分類器を使用して画像ファイルから顔部分を検出する
@@ -58,15 +63,15 @@ def predict():
                 for x,y,w,h in face:
                     face_cut = img[y:y+h, x:x+w]
         
-                face_cut_resized = cv2.resize(face_cut, (50,  50))
+                face_cut_resized = cv2.resize(face_cut, (size,  size))
                 img_nad = img_to_array(face_cut_resized)/255
                 img_nad = img_nad[None, ...]
 
-                with graph.as_default():
-                    label=["Beautiful", "normal"]
-                    pred = model.predict(img_nad, batch_size=1, verbose=0)
-                    pred_label = label[np.argmax(pred[0])]
-                    result = "She is {} for Makoto Nakai".format(pred_label)
+#                 with graph.as_default():
+                label=["Beautiful", "normal"]
+                pred = model.predict(img_nad, batch_size=1, verbose=0)
+                pred_label = label[np.argmax(pred[0])]
+                result = "She is {} for Makoto Nakai".format(pred_label)
         
             else:
                 result = "Face detection failed. Please try another image"
@@ -80,4 +85,4 @@ def uploaded_file(filename):
     return send_from_directory(app.config['UPLOAD_FOLDER'], filename)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(threaded=False, debug=True)
